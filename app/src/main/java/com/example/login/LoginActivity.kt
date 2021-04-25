90c8a7bbb39a7beb7f3ef8088fb6832d7ede564c
gvillavicencios <gvillavicencios@outlook.com>
2021-04-24 22:39:42 -0600

    Segunda Tarea

--- app/src/main/java/com/example/login/LoginActivity.kt
+++ app/src/main/java/com/example/login/LoginActivity.kt
@@ -1,24 +1,33 @@
 package com.example.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import com.google.android.material.textfield.TextInputEditText
 
 class LoginActivity : AppCompatActivity() {
 
 
     //Object Declare
-
     private lateinit var txtEmailObj : TextInputEditText
     private lateinit var txtPwdObj : TextInputEditText
-     private lateinit var btnLogIn : Button
+    private lateinit var btnLogIn : Button
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         this.supportActionBar?.hide();
         setContentView(R.layout.activity_login)
+
+        // Get Values
+        txtEmailObj= findViewById(R.id.emailInput)
+        txtPwdObj= findViewById(R.id.passwordInput)
+
+        //Get Object and try the intent
+        btnLogIn= findViewById(R.id.btnLogin)
+
 
     }
 }
