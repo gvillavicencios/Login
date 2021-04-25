6989739317866d2070424709f20fce3ff1252f5f
gvillavicencios <gvillavicencios@outlook.com>
2021-04-24 22:16:42 -0600

    Segunda Tarea

--- app/src/main/java/com/example/login/LoginActivity.kt
+++ app/src/main/java/com/example/login/LoginActivity.kt
@@ -2,11 +2,23 @@ package com.example.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 
 class LoginActivity : AppCompatActivity() {
+
+
+    //Object Declare
+
+    private lateinit var txtEmailObj : TextInputEditText
+    private lateinit var txtPwdObj : TextInputEditText
+     private lateinit var btnLogIn : Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         this.supportActionBar?.hide();
         setContentView(R.layout.activity_login)
+
     }
 }
---
+++ app/src/main/java/com/example/login/UserLoginData.kt
@@ -0,0 +1,28 @@
+package com.example.login
+
+import android.os.Parcel
+import android.os.Parcelable
+
+data class UserLoginData(val email:String, val passwordKey: String): Parcelable {
+    constructor(parcel: Parcel) : this(parcel.readString()?:"", parcel.readString()?:"") {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(email)
+        parcel.writeString(passwordKey)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<UserLoginData> {
+        override fun createFromParcel(parcel: Parcel): UserLoginData {
+            return UserLoginData(parcel)
+        }
+
+        override fun newArray(size: Int): Array<UserLoginData?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
